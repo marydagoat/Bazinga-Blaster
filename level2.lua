757ad91b6827f0639e10941d88b08e3d121c2577
Maryanne .S <202732611+marydagoat@users.noreply.github.com>
2026-07-09 21:33:23 -0500

    Added more code to level2

--- level2.lua
+++ level2.lua
@@ -1,2 +1,14 @@
 --level 2 :33
 local level2 = {}
+
+--Variables assigned for level 2
+local level2Squares = {}
+local level2Score = 0
+
+local function spawnLevel2Square()
+    local square = {
+        x = love.math.random(50, love.graphics.getWidth() - 50)
+        y = love.math.random(50, love.graphics.getHeight() - 50)
+    }
+    table.insert(level2Squares, square)
+end 
